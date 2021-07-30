36dde295e2f34e210029e8975fa5e247825a0e83
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-30 14:13:57 -0400

    Update MerkleListedToken.sol
    nit

--- contracts/token/MerkleListedToken.sol
+++ contracts/token/MerkleListedToken.sol
@@ -3,48 +3,66 @@ pragma solidity ^0.8.6;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/cryptography/MerkleProof.sol";
 
-/// @notice This contract manages function access control, adapted from @boringcrypto (https://github.com/boringcrypto/BoringSolidity).
-contract Ownable {
-    address public owner; 
-    address public pendingOwner;
-    
-    event TransferOwnership(address indexed from, address indexed to); 
-    event TransferOwnershipClaim(address indexed from, address indexed to);
-    
-    /// @notice Initialize contract.
-    constructor() {
-        owner = msg.sender;
-        emit TransferOwnership(address(0), msg.sender);
+// Allows anyone to claim a token if they exist in a merkle root.
+interface IMerkleDistributor {
+    // Returns the address of the token distributed by this contract.
+    function token() external view returns (address);
+    // Returns the merkle root of the merkle tree containing account balances available to claim.
+    function merkleRoot() external view returns (bytes32);
+    // Returns true if the index has been marked claimed.
+    function isClaimed(uint256 index) external view returns (bool);
+    // Claim the given amount of the token to the given address. Reverts if the inputs are invalid.
+    function claim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof) external;
+
+    // This event is triggered whenever a call to #claim succeeds.
+    event Claimed(uint256 index, address account, uint256 amount);
+}
+
+contract MerkleDistributor is IMerkleDistributor {
+    address public immutable override token;
+    bytes32 public immutable override merkleRoot;
+
+    // This is a packed array of booleans.
+    mapping(uint256 => uint256) private claimedBitMap;
+
+    constructor(address token_, bytes32 merkleRoot_) public {
+        token = token_;
+        merkleRoot = merkleRoot_;
     }
-    
-    /// @notice Access control modifier that requires modified function to be called by `owner` account.
-    modifier onlyOwner {
-        require(msg.sender == owner, 'Ownable:!owner');
-        _;
-    } 
-    
-    /// @notice The `pendingOwner` can claim `owner` account.
-    function claimOwner() external {
-        require(msg.sender == pendingOwner, 'Ownable:!pendingOwner');
-        emit TransferOwnership(owner, msg.sender);
-        owner = msg.sender;
-        pendingOwner = address(0);
+
+    function isClaimed(uint256 index) public view override returns (bool) {
+        uint256 claimedWordIndex = index / 256;
+        uint256 claimedBitIndex = index % 256;
+        uint256 claimedWord = claimedBitMap[claimedWordIndex];
+        uint256 mask = (1 << claimedBitIndex);
+        return claimedWord & mask == mask;
     }
-    
-    /// @notice Transfer `owner` account.
-    /// @param to Account granted `owner` access control.
-    /// @param direct If 'true', ownership is directly transferred. 
-    function transferOwnership(address to, bool direct) external onlyOwner {
-        if (direct) {
-            owner = to;
-            emit TransferOwnership(msg.sender, to);
-        } else {
-            pendingOwner = to;
-            emit TransferOwnershipClaim(msg.sender, to);
-        }
+
+    function _setClaimed(uint256 index) private {
+        uint256 claimedWordIndex = index / 256;
+        uint256 claimedBitIndex = index % 256;
+        claimedBitMap[claimedWordIndex] = claimedBitMap[claimedWordIndex] | (1 << claimedBitIndex);
+    }
+
+    function claim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof) external override {
+        require(!isClaimed(index), 'MerkleDistributor: Drop already claimed.');
+
+        // Verify the merkle proof.
+        bytes32 node = keccak256(abi.encodePacked(index, account, amount));
+        require(MerkleProof.verify(merkleProof, merkleRoot, node), 'MerkleDistributor: Invalid proof.');
+
+        // Mark it claimed and send the token.
+        _setClaimed(index);
+        require(IERC20(token).transfer(account, amount), 'MerkleDistributor: Transfer failed.');
+
+        emit Claimed(index, account, amount);
     }
 }
 
 contract MerkleListedToken is Ownable {
     
 }
+
+
+
+
